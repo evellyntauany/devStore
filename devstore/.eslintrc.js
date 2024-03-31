f0be1fc107eb53269b4e2a9b1331c521b6ba8bfe
Evellyn <evirodrigues10@gmail.com>
2024-03-31 20:48:37 -0300

    finalizando projeto

--- devstore/.eslintrc.js
+++ devstore/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['next/core-web-vitals', '@rocketseat/eslint-config/next'],
+  rules: { '@typescript-eslint/no-namespace': 'off' },
   globals: {
     JSX: 'readonly',
   },
